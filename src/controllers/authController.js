71a2010b1935981045e2a3e44f9f3c00633dbdf6
David Dobrovolny <david.dobrovolny@plus4u.net>
2025-03-15 10:40:59 +0100

    add authController with register endpoint

---
+++ src/controllers/authController.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken,
+} = require("../services/authService");
+const User = require("../models/User");
+
+// Registrace
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Uživatel již existuje." });
+    }
+
+    user = new User({ email, password });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const accessToken = generateAccessToken(user.id);
+    const refreshToken = generateRefreshToken(user.id);
+
+    // Uložení refresh tokenu do databáze
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.json({ accessToken, refreshToken });
+  } catch (error) {
+    res.status(500).json({ message: "Chyba serveru." });
+  }
+});
+
+module.exports = router;
