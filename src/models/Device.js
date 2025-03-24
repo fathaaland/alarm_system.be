182ec28f5f7c3192396f7366b658b43a65469f6e
David Dobrovolny <david.dobrovolny@plus4u.net>
2025-03-24 15:09:32 +0100

    correct Device schema

--- src/models/Device.js
+++ src/models/Device.js
@@ -2,13 +2,25 @@ const mongoose = require("mongoose");
 const { deviceDB } = require("../db/dbConnection");
 
 const deviceSchema = new mongoose.Schema({
-  device_name: {
+  name: {
     type: String,
     required: true,
   },
   device_type: {
     type: String,
     required: true,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  alarm_triggered: {
+    type: Number,
+    default: 0,
+  },
+  household_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Household",
   },
   createdAt: {
     type: Date,
