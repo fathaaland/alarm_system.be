e3946e425a195fbc5dfe39deb08516a5cd379d6a
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-03-29 14:31:38 +0100

    preparing routes for add and remove user

--- src/routes/householdRoutes.js
+++ src/routes/householdRoutes.js
@@ -4,11 +4,25 @@ const householdController = require("../controllers/householdController");
 const authMiddleware = require("../middlewares/auth");
 
 router.post("/create", authMiddleware, householdController.createHousehold);
+
 router.get("/", authMiddleware, householdController.getHousehold);
+
 router.delete(
   "/delete/:id",
   authMiddleware,
   householdController.deleteHousehold
+);
+
+router.put(
+  "/add-user/:id",
+  authMiddleware,
+  householdController.addUserHousehold
+);
+
+router.put(
+  "/remove-user/:id",
+  authMiddleware,
+  householdController.removeUserHousehold
 );
 
 module.exports = router;
