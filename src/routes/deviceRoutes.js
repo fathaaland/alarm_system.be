0a7e8957958bd0f97a162ddacf3fb6771b700a2d
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-04-03 18:37:43 +0200

    implementing creating device is finish

--- src/routes/deviceRoutes.js
+++ src/routes/deviceRoutes.js
@@ -4,5 +4,10 @@ const deviceController = require("../controllers/deviceController");
 const authMiddleware = require("../middlewares/auth");
 
 router.post("/create", authMiddleware, deviceController.createDevice);
+router.delete(
+  "/delete/:deviceId",
+  authMiddleware,
+  deviceController.deleteDevice
+);
 
 module.exports = router;
