ba23cf8f57d5bbfa0594cae3e57e07e6319ab5ef
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-04-15 15:22:08 +0200

    corret replacing admin service in correct folder

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const Admin = require("../models/Admin");
 
 const generateAdminAccessToken = (adminId) => {
   return jwt.sign({ id: adminId }, process.env.JWT_FOR_ADMIN, {
@@ -20,4 +22,22 @@ module.exports = {
   generateAdminAccessToken,
   generateAdminRefreshToken,
   verifyAdminRefreshToken,
+};
+
+exports.admin = async (adminData) => {
+  try {
+    const newAdmin = new Admin({
+      firstName: adminData.firstName,
+      lastName: adminData.lastName,
+      password: adminData.lastName,
+      email: adminData.email,
+      role: adminData.role,
+      refreshToken: adminData.refreshToken,
+    });
+
+    await newAdmin.save();
+    return newAdmin;
+  } catch (error) {
+    throw error;
+  }
 };
