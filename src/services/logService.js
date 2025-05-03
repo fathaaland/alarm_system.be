35df8eb90cd437d8ac4737d1b281b0d4faa59094
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-05-03 21:29:45 +0200

    adding createLog in service

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,12 +1,23 @@
 const Log = require("../models/Log");
+const Household = require("../models/Household");
+const { logDB } = require("../db/dbConnection");
 
-exports.getLogs = async (householdId) => {
+exports.createLog = async (logData) => {
   try {
-    const logs = await Log.find({ householdId: householdId });
-    return logs;
+    const newLog = new logDB({
+      name: logData.name,
+      type: logData.type,
+      active: logData.active,
+      alarm_triggered: logData.alarm_triggered,
+      householdId: logData.householdId,
+      hw_id: logData.hw_id,
+      createdBy: logData.createdBy,
+      isAdminCreated: true,
+    });
+
+    await newLog.save();
+    return newLog;
   } catch (error) {
     throw error;
   }
 };
-
-module.exports = exports;
